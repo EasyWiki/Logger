9135c3bb366915467ebd2a034721372c90c5edf6
Mathias Maes <mathiasmaesdev@gmail.com>
2019-12-28 19:25:04 +0100

    FIX: Merge function

--- src/OptionBuilder.ts
+++ src/OptionBuilder.ts
@@ -18,14 +18,19 @@ export function OptionBuilder(options?: LoggerOptions)
 function Merge(options1: any, options2: any)
 {
     const keys = Object.keys(options1);
+    let newOptions = {} as any;
     
     for (const key in keys)
     {
-        if (options2.hasOwnProperty(key))
+        if (!options2[key])
         {
-            options1[key] = options2[key];
+            newOptions[key] = options1[key];
+        }
+        else
+        {
+            newOptions[key] = options2;
         }
     }
 
-    return options1;
+    return newOptions;
 }
